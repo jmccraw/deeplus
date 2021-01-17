e1fb39c61d0a1af199d19735565d44530c4c3d57
Justin McCraw <jmccraw1@gmail.com>
2021-01-17 12:32:34 -0500

    Clean up the index a bit

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
 import initNav from './components/nav';
 import initContainers from './containers';
 
-// TODO FIXME See if need any globals
-// declare global {
-//   interface Window {
-
-//   }
-// }
-
-
 const App = () => {
   initNav();
   initContainers();
